a06fc3ccb00d86143620d875c9a1c1df3dfa89f0
nhu phuong <nhuphuong.khtn@gmail.com>
2022-06-03 16:22:28 +0700

    fix subyt

--- src/components/vdYtSub.jsx
+++ src/components/vdYtSub.jsx
@@ -1,106 +1,99 @@
-// this file is converted from javascript to reactjs so some code is not optimized
 import React, { useEffect, useState } from "react";
-// import LineSub from "./childCpn/lineSub.jsx";
-// import YTSubtitles from "youtube-subtitles-downloader";
 import '../common/style.css';
 import _ from 'lodash';
 import { Sub } from './childCpn/subtitle.jsx'
-// import YouTube from 'react-youtube';
 
 let player;
-var timerId;
+let interval
 const YoutubeSub = () => {
 
-    const [arrSub, setArrSub] = useState(Array());
-    const [timeReplay, setTimeReplay] = useState(10000000);
-    const [isReplay, setIsReplay] = useState(false);
-    const [timeStart, setTimeStart] = useState(0);
-    const [isChangeSub, setIsChangeSub] = useState(false);
-    // const [idVideo, setIdVideo] = useState('');
+    const [arrSub, setArrSub] = useState([]);
+
+    const [isReplay, setIsReplay] = useState(true);
+    const CONTROL_REPLAY = 'Replay';
 
     useEffect(() => {
-
+        console.log("useEffect[]:");
         if (!window.YT) { // If not, load the script asynchronously
             const tag = document.createElement('script');
             tag.src = 'https://www.youtube.com/iframe_api';
-      
+
             // onYouTubeIframeAPIReady will load the video after the script is loaded
             window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-      
+
             const firstScriptTag = document.getElementsByTagName('script')[0];
             firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-      
-          } else { // If script is already there, load the video directly
+
+        } else { // If script is already there, load the video directly
             // loadVideo();
             onYouTubeIframeAPIReady();
-          }
+        }
+        document.getElementById('media-sub').value = `0:04
+in sequel server so let's begin in front
+0:12
+of me I have a JSON object which
+0:14
+contains information about superheroes
+0:16
+like a squad like members and a list of
+0:21
+powers for each member and we're going
+0:23
+to solve this jason in a sequel table
+0:25
+and we are going to extract some fields
+0:27
+from here so basically we are going to
+0:30
+parse some JSON object and jason erase i
+0:33
+have added the json object in in a
+0:37
+sequel table and i've also added another
+0:40
+object which is not correctly formatted
+0:44
+and the reason why i've added the second`;
+        return () => {
+            console.log("destroy interval:"+interval);
+            clearInterval(interval);
+        };
+       
     }, []);
-    const onYouTubeIframeAPIReady=()=> {
+
+
+    const onYouTubeIframeAPIReady = () => {
         player = new window.YT.Player('player', {
-          height: 390,
-          width: 640,
-          videoId: "",
-          playerVars: {
-            'playsinline': 1
-          },
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
+            height: 390,
+            width: 640,
+            videoId: "0Zm41iZzbnI",
+            playerVars: {
+                'playsinline': 1
+            },
+            events: {
+                'onReady': onPlayerReady,
+                // 'onStateChange': onPlayerStateChange
+            }
         });
-      }
-      const onPlayerReady=(event)=> {
+    }
+    const onPlayerReady = (event) => {
         event.target.playVideo();
-      }
-      var done = false;
-      const onPlayerStateChange=(event)=> {
-        if (event.data === window.YT.PlayerState.PLAYING && !done) {
-        //   setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      const stopVideo=() =>{
-        player.stopVideo();
-      }
-
+    }
     
-    useEffect(() => {
-        if(isChangeSub === true){
-            clearInterval(timerId)
-        }
-        // if (isReplay===true) {
-            console.log('replay');
-             timerId= setInterval(() => {
-                player.seekTo(timeStart, true);
-                setIsChangeSub(false);
-                // setTimeReplay(timeReplay+1)
-            }, (timeReplay));
-        // }else{
-        //     clearInterval(timerId);
-        // }
-
-    }, [timeReplay]);
-
-    useEffect(() => {
- if (isReplay===false) {
-    clearInterval(timerId);
- }
-    }, [isReplay]);
-
     const onResize = () => {
-       player.setSize(640,390);
+        player.setSize(640, 390);
     };
     const onPressBtnVerySmall = () => {
-        player.setSize(100,80);
+        player.setSize(100, 80);
     };
     const onPressBtnSmall = () => {
-        player.setSize(70,50);
+        player.setSize(70, 50);
     };
     const onPressBtnMedium = () => {
-        player.setSize(1200,700);
+        player.setSize(1200, 700);
     };
     const onPressBtnBig = () => {
-        player.setSize(1200,700);
+        player.setSize(1200, 700);
     };
     const onHideAll = () => {
         document.getElementById('control').style.display = "none";
@@ -114,84 +107,94 @@ const YoutubeSub = () => {
     const loadSub = () => {
         var txtSub = document.getElementById('media-sub').value;
         var lineSubArr = txtSub.split('\n');
-        let count =1;
+        let count = 1;
         let tempTime = '';
         let arrTemp = [];
-        lineSubArr.forEach((line, index )=> {
-            if(count===1){
-             tempTime = line;
+        lineSubArr.forEach((line, index) => {
+            if (count === 1) {
+                tempTime = line;
             }
-            if(count===2){
-               arrTemp.push(new Sub(tempTime, line));
-                count=0;
+            if (count === 2) {
+                arrTemp.push(new Sub(tempTime, line));
+                count = 0;
             }
             count++;
-            
+
         });
         setArrSub(arrTemp);
     };
 
     const LineSub = (props) => {
         return (
-            <div role ='button' className ='sub-item'  onClick={(e) => onClickSub(props.time, props.value)}>
-                key ={props.time}   value ={props.value} 
+            <div role='button' className='sub-item' onClick={(e) => onClickSub(props.time, props.value)}>
+                key ={props.time}   value ={props.value}
             </div>
         )
     }
     const onClickSub = (time, value) => {
-        console.log(time);
-        console.log(value);
-        setIsChangeSub(true);
-        let timeSecond = time.split(':').reduce((acc,time) => (60 * acc) + +time);
-        setTimeStart(timeSecond);
-        let nextIndex=arrSub.length-1;
-        for(let i=0; i<arrSub.length; i++){
-            if(_.isEqual(arrSub[i].time, time)){
-                nextIndex = i+1;
+        console.log("interval1:"+interval);
+        clearInterval(interval);
+        let timestart = time.split(':').reduce((acc, time) => (60 * acc) + +time);
+        let nextIndex = arrSub.length - 1;
+        for (let i = 0; i < arrSub.length; i++) {
+            if (_.isEqual(arrSub[i].time, time)) {
+                nextIndex = i + 1;
             }
         }
-        let nextTime =arrSub[nextIndex].time.split(':').reduce((acc,time) => (60 * acc) + +time);
-        console.log(timeSecond);
+        let nextTime = arrSub[nextIndex].time.split(':').reduce((acc, time) => (60 * acc) + +time);
+        console.log(timestart);
         console.log(nextTime);
-        setIsReplay(true);
-        setTimeReplay((nextTime-timeSecond)*1000);
-        // let tArr= arrSub.filter(item=>_.isEqual(item.time, time));
-        
-        // console.log(player.getCurrentTime());
-        player.seekTo(timeSecond, true);
+        // setTimeStart(timestart);
+        // setNextTime(nextTime);
+        player.seekTo(timestart, true);
+        console.log("onClickSub");
 
+            interval = setInterval(() => {
+                let currTime = player.getCurrentTime().toString().substring(0, 2);
+                if (_.isEqual(currTime, nextTime.toString()) && isReplay === true) {
+                    console.log("player.seekTo(timeStart, true);:"+timestart);
+                    player.seekTo(timestart, true)
+                }
+            }, 1000);
+            console.log("interval2:"+interval);
+            
+        
     };
     const onProcess = () => {
         var txtSrcMedia = document.getElementById('txtSrcMedia').value;
-        var url =  txtSrcMedia.substring(txtSrcMedia.lastIndexOf('/') + 1, txtSrcMedia.length).trim();
-        
+        var url = txtSrcMedia.substring(txtSrcMedia.lastIndexOf('/') + 1, txtSrcMedia.length).trim();
         player.loadVideoById(url, 0);
-        // setIdVideo(url);
     };
-    const onStop = () => {
-        setIsReplay(false);
-    };
-
+    const onChangeReplay = (value) => {
+        // setIsUseVoice(value);
+        if (_.isEqual(value, CONTROL_REPLAY)) {
+            setIsReplay(false);
+        } else {
+            clearInterval(interval);
+            setIsReplay(false);
+        }
+    }
     return (
-        
+
         <div className='media-left'>
-            {/* <iframe title="this is a video, clear!" width="420" height="315" id='iFMedia' src="">
-            </iframe> <br /> */}
             <div id="player"></div>
-            {/* <YouTube id='iFMedia' videoId="8UVNT4wvIGY" opts={opts} onReady={_onReady} /> */}
             <div id=''>
                 <div className=''>
                     <div className=''>
-                    <div id = 'sub-control' >
-                        <div>
-                        {arrSub.map((item, index) => <LineSub  key ={`${item.time}${item.value}`}
-                        time={item.time}
-                        value={item.value}
-                        // onClick={(e) => onClickSub(e)}
-                    />)}
+                        <div id='sub-control' >
+                            <div>
+                                {arrSub.map((item, index) => <LineSub key={`${item.time}${item.value}`}
+                                    time={item.time}
+                                    value={item.value}
+                                />)}
+                            </div>
                         </div>
-                    </div>
-                        <input type='submit' value="Stop" id='btnStop' onClick={() => onStop()} />
+                        <select   onChange={(e) => {
+                            onChangeReplay(e.target.value)
+                        }}>
+                            <option value={CONTROL_REPLAY}>Replay</option>
+                            <option value="None">None</option>
+                        </select>
                         <br />
                         <input type="text" id="txtSrcMedia" /> <br />
                         <input type='submit' value="Load" id='btnExecute' onClick={() => onProcess()} />
@@ -214,7 +217,7 @@ const YoutubeSub = () => {
                 <div className='option-right'> <br />
                 </div>
                 <input type='submit' value="loadSub" id='btnLoadSube' onClick={() => loadSub()} /><br />
-                <textarea  id='media-sub'></textarea>
+                <textarea id='media-sub'></textarea>
 
             </div>
             <br />
